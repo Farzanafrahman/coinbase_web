b9fbdedd2e68128c963a8ff9f625fb1a994e7b18
Farzanafrahman <zanaavine.89@gmail.com>
2021-10-13 07:04:32 +0600

    footer modified

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,22 +1,24 @@
 <template>
   <div class="footer">
+    <b-container>
       <b-row>
         <b-col class="first-col">
-          <img src="@/assets/img/footer_logo.png" alt="">
-          <p> Fusce ut elit aliquet, fermentum leo vel, tempus nunc. Fusce eu rhoncus augue. 
+          <img class="logo-img mb-3" src="@/assets/img/footer_logo.png" alt="">
+          <p class="content my-2"> Fusce ut elit aliquet, fermentum leo vel, tempus nunc. Fusce eu rhoncus augue. 
             Fusce vel metus pharetra, fermentum
           </p>
-          <div class="icons">
+          <div class="icons my-2">
             <img src="@/assets/img/icons/google+.svg" alt="">
             <img src="@/assets/img/icons/facebook.svg" alt="">
             <img src="@/assets/img/icons/twitter.svg" alt="">
             <img src="@/assets/img/icons/instagram.svg" alt="">
           </div>
+          <p class="copyright">&copy; 2019 Designed by Cr8tiv_yemmy</p>
         </b-col>
         <b-col class="second-col">
           <b-row>
             <b-col>
-              <h5>CoinBase Links</h5>
+              <p class="mb-3 pt-2">CoinBase Links</p>
               <ul>
                 <li>Home</li>
                 <li>About Us</li>
@@ -25,22 +27,32 @@
                 <li>Contact Us</li>
               </ul>
             </b-col>
-            <b-col>
-              <h5>Contact Us</h5>
+            <b-col class="position-relative">
+              <p class="mb-3 pt-2">Contact Us</p>
               <ul>
                 <li>(+234)137632128</li>
                 <li>Coinbase@yahoo.com</li>
                 <li>Address goes here</li>
-                <li>Some address</li>
-                <li>Download Our App</li>
+                <li class="download">Download Our App</li>
               </ul>
             </b-col>
           </b-row>
         </b-col>
         <b-col class="third-col">
-          <p>CoinBase Instagram</p>
+          <p class="mb-3 pt-2">CoinBase Instagram</p>
+          <div class="grid-container">
+            <div class="grid-item"></div>
+            <div class="grid-item">+</div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+            <div class="grid-item"></div>
+          </div>
         </b-col>
       </b-row>
+    </b-container> 
   </div>
 </template>
 
@@ -54,12 +66,12 @@ export default {
   .footer {
     background-color: #333;
     color: #fff;
-    padding: 40px 40px 40px 80px;
+    padding: 40px 0;
     text-align: left;
+    position: relative;
+    align-items: stretch;
     .first-col {
-      p {
-        max-width: 70%;
-        padding: 20px 40px 0 0;
+      .content {
         font-size: 10px;
         color: #a2a2a2;
         line-height: 18px;
@@ -67,20 +79,52 @@ export default {
       .icons img {
         padding-right: 16px;
       }
+      .copyright {
+        position: absolute;
+        bottom: 10px;
+        font-size: 8px;
+        color: #989898;
+        margin-bottom: 0;
+      }
     }
     .second-col {
-      h5 {
-        padding-bottom: 18px;
+      p {
         font-size: 14px;
       }
       ul {
         padding-left: 0;
+        margin-bottom: 0;
         li {
           font-size: 10px;
           color: #a2a2a2;
           line-height: 20px;
         }
+        .download {
+          position: absolute;
+          bottom: 0;
+          font-size: 10px;
+          color: #a2a2a2;
+        }
       }
-    } 
+    }
+    .third-col {
+      p {
+        font-size: 14px;
+      }
+      .grid-container {
+        display: grid;
+        grid-template-columns: repeat(4, 60px);
+        grid-template-rows: repeat(2, 60px);
+        background-color: #333;
+        grid-gap: 10px;
+        .grid-item {
+          background-color: #a2a2a2;
+          text-align: center;
+          font-size: 34px;
+          font-weight: 100;
+          line-height: 60px;
+        }
+      }
+    }
   }
 </style>
