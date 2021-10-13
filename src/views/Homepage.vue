6177933af2262f4483faa2c367462cb92720c87d
Farzanafrahman <zanaavine.89@gmail.com>
2021-10-13 08:38:20 +0600

    newsletter implemented

--- src/views/Homepage.vue
+++ src/views/Homepage.vue
@@ -4,6 +4,7 @@
     <IntroSection />
     <WelcomeSection />
     <Rating />
+    <NewsLetter />
     <Footer />
   </div>
 </template>
@@ -13,6 +14,7 @@ import Header from "../components/Header.vue"
 import IntroSection from "../components/IntroSection.vue"
 import WelcomeSection from "../components/WelcomeSection.vue"
 import Rating from "../components/Rating.vue"
+import NewsLetter from "../components/NewsLetter.vue"
 import Footer from "../components/Footer.vue"
 
 export default {
@@ -22,9 +24,9 @@ export default {
     IntroSection,
     WelcomeSection,
     Rating,
+    NewsLetter,
     Footer,
-  }
-  
+  } 
 }
 </script>
 
